9be792b6e5a7bd9ac68af15b22bf0534d353cefb
Dan Norman <buzzcutnorman@gmail.com>
2023-04-07 13:18:45 -0700

    tidied up a little

--- tap_stackoverflow_sampledata/client.py
+++ tap_stackoverflow_sampledata/client.py
@@ -1,11 +1,13 @@
-"""Custom client handling, including stackoverflow-sampledataStream base class."""
+"""Custom client handling, including
+StackOverflowSampleDataStream base class."""
 
 import os
 
-from typing import Any, Dict, Optional, Union, List, Iterable
+from typing import Optional, Iterable
 
 from singer_sdk.streams import Stream
 from lxml import etree
+
 
 class StackOverflowSampleDataStream(Stream):
     """Stream class for stackoverflow-sampledata streams."""
@@ -14,62 +16,70 @@ class StackOverflowSampleDataStream(Stream):
     file_name: str = None
 
     def __init__(self, *args, **kwargs):
-        """Init StackOverflowSampleDataStream"""
+        """Init StackOverflowSampleDataStream
+
+            Args:
+                args: Class constructor positional arguments.
+                kwargs: Class constructor keyword arguments.
+        """
 
         super().__init__(*args, **kwargs)
-    
+
     def get_records(self, context: Optional[dict]) -> Iterable[dict]:
         """Return a generator of row-type dictionary objects.
 
-        The optional `context` argument is used to identify a specific slice of the
-        stream if partitioning is required for the stream. Most implementations do not
-        require partitioning and should ignore the `context` argument.
+        Args:
+            context: If partition context is provided, will read specifically
+                from this data slice.
+
+        Yields:
+            One dict per record.
         """
-        # Check if the file_path has been populated 
+        # Check if the file_path has been populated
         # if it hasn't fill run get_file_path to populate it
         if self.file_path:
             data_file = self.file_path
-        else: 
+        else:
             self.get_file_path()
             data_file = self.file_path
 
         # Grab the rows from the xml file using by
         # opening the file using using a iternation parser
         for event, element in etree.iterparse(data_file):
-            
-            # Dictionaries to place and raw refined rows 
+
+            # Dictionaries to place and raw refined rows
             xml_row = dict()
             row = dict()
-            
+
             # The data is held a attributes to each sub root row
             # We grab each attibute item and type it according to the schmea
             for attribute in element.attrib.items():
                 # Each attribut is a key and value pair "ID"=1
                 key: str = attribute[0]
                 value: str = attribute[1]
-                
+
                 # We try to grab to schema type property for the attibute
-                # Then type the value. If a type can no be grabbed we 
-                # pass the default string type. 
+                # Then type the value. If a type can no be grabbed we
+                # pass the default string type.
                 try:
                     value_type: str = self.schema['properties'][key]['type'][0]
                 except:
                     value_type: str = 'string'
 
                 if value_type == 'integer':
-                    value = int(value) 
-                
+                    value = int(value)
+
                 # We add the key value pair to the xml_row dictionary
                 xml_row[key] = value
 
             # Blank list to hold al the Primary Key columns
             column_names = list()
-            
+
             #  We grab the column names from the Stream Schema
             #  Then append them to the columns names list
             for column in self.schema['properties'].keys():
                 column_names.append(str(column))
-            
+
             # We use the columns list to add columns that didn't
             # have any data in the xml row and set the
             # column value to none or null
@@ -78,30 +88,29 @@ class StackOverflowSampleDataStream(Stream):
                     row[column] = xml_row[column]
                 else:
                     row[column] = None
-            
-            # We are under the assuption that 
+
+            # We are under the assuption that
             # primary key(s) have valus present
             primary_key_not_null = True
 
             # Check each primary key in the row to make
             # sure it has a value if one does not have value
-            # set the primary key not null flag to False   
+            # set the primary key not null flag to False
             for key_col in self.primary_keys:
                 if row.get(key_col) is None:
                     primary_key_not_null = False
-            
+
             # If the primary key not null flag is True
             # yeild the row as a dictrionary
             if primary_key_not_null:
                 yield dict(row)
-            
+
             # We clear the element so we don't use memory
             # to hold rows we won't need again
             element.clear()
             if element.getparent() is not None:
                 element.getparent().remove(element)
 
-    
     def get_file_path(self):
         """Return a list of file paths to read.
         This tap accepts file names and directories so it will detect
@@ -110,8 +119,8 @@ class StackOverflowSampleDataStream(Stream):
         # Cache file paths so we dont have to iterate multiple times
         if self.file_path and (os.path.basename(self.file_path) == self.file_name):
             return self.file_path
-    
-        # Check that the file_path from the meltano.yaml exists 
+
+        # Check that the file_path from the meltano.yaml exists
         # if it isn't we alert there is an issue
         file_path = self.config.get("stackoverflow_data_directory")
         if not os.path.exists(file_path):
@@ -119,7 +128,7 @@ class StackOverflowSampleDataStream(Stream):
 
         # Check if the file_path is a directory or a file
         # Add the file Streams file_name to a direcotory path
-        # Pass along file_path if it points to the Stream's file_name  
+        # Pass along file_path if it points to the Stream's file_name
         full_file_path: str = None
         if os.path.isdir(file_path):
             clean_file_path = os.path.normpath(file_path) + os.sep
@@ -137,17 +146,16 @@ class StackOverflowSampleDataStream(Stream):
                 f"Stream '{self.name}' has no acceptable files. \
                     See warning for more detail."
             )
-    
-    
+
     def is_valid_filename(self, file_path: str) -> bool:
         """Return a boolean of whether the path is a file includes an XML extension."""
         is_valid = True
-        
+
         # Check to see if this is a direcotry turn
-        # the is valid flag to false and 
+        # the is valid flag to false and
         # say you are skipping the file
         if os.path.isdir(file_path):
-            is_valid = False 
+            is_valid = False
             self.logger.info(f"Skipping folder {file_path}")
 
         # Check to see if the if the file has a .xml extention
@@ -158,4 +166,3 @@ class StackOverflowSampleDataStream(Stream):
             self.logger.warning(f"Skipping non xml file '{file_path}'")
 
         return is_valid
-
--- tap_stackoverflow_sampledata/streams.py
+++ tap_stackoverflow_sampledata/streams.py
@@ -1,16 +1,13 @@
 """Stream type classes for tap-stackoverflow-sampledata."""
-
-from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
-
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_stackoverflow_sampledata.client import StackOverflowSampleDataStream
 
+
 class BadgesStream(StackOverflowSampleDataStream):
     """Define custom stream."""
     name = "badges"
-    file_name = "Badges.xml"   
+    file_name = "Badges.xml"
     primary_keys = ["Id"]
     replication_key = None
     schema = th.PropertiesList(
@@ -22,21 +19,23 @@ class BadgesStream(StackOverflowSampleDataStream):
         th.Property("TagBased", th.BooleanType),
     ).to_dict()
 
+
 class CommentsStream(StackOverflowSampleDataStream):
     """Define custom stream."""
     name = "comments"
     file_name = "Comments.xml"
     primary_keys = ["Id"]
     replication_key = None
-    schema = th.PropertiesList(    
+    schema = th.PropertiesList(
         th.Property("Id", th.IntegerType),
         th.Property("PostId", th.IntegerType),
-        th.Property("Score", th.IntegerType), 
+        th.Property("Score", th.IntegerType),
         th.Property("Text", th.StringType),
         th.Property("CreationDate", th.DateTimeType),
         th.Property("UserId", th.IntegerType),
         th.Property("ContentLicense", th.StringType),
     ).to_dict()
+
 
 class PostLinksStream(StackOverflowSampleDataStream):
     """Define custom stream."""
@@ -45,12 +44,13 @@ class PostLinksStream(StackOverflowSampleDataStream):
     primary_keys = ["Id"]
     replication_key = None
     schema = th.PropertiesList(
-        th.Property("Id", th.IntegerType), 
+        th.Property("Id", th.IntegerType),
         th.Property("CreationDate", th.DateTimeType),
         th.Property("PostId", th.IntegerType),
         th.Property("RelatedPostId", th.IntegerType),
         th.Property("LinkTypeId", th.IntegerType),
     ).to_dict()
+
 
 class PostsStream(StackOverflowSampleDataStream):
     """Define custom stream."""
@@ -59,9 +59,9 @@ class PostsStream(StackOverflowSampleDataStream):
     primary_keys = ["Id"]
     replication_key = None
     schema = th.PropertiesList(
-        th.Property("Id", th.IntegerType), 
-        th.Property("PostTypeId", th.IntegerType), 
-        th.Property("AcceptedAnswerId", th.IntegerType), 
+        th.Property("Id", th.IntegerType),
+        th.Property("PostTypeId", th.IntegerType),
+        th.Property("AcceptedAnswerId", th.IntegerType),
         th.Property("CreationDate", th.DateTimeType),
         th.Property("Score", th.IntegerType),
         th.Property("ViewCount", th.IntegerType),
@@ -80,6 +80,7 @@ class PostsStream(StackOverflowSampleDataStream):
         th.Property("ContentLicense", th.StringType),
     ).to_dict()
 
+
 class TagsStream(StackOverflowSampleDataStream):
     """Define custom stream."""
     name = "tags"
@@ -87,12 +88,13 @@ class TagsStream(StackOverflowSampleDataStream):
     primary_keys = ["Id"]
     replication_key = None
     schema = th.PropertiesList(
-        th.Property("Id", th.IntegerType), 
-        th.Property("TagName", th.StringType), 
-        th.Property("Count", th.IntegerType),  
-        th.Property("ExcerptPostId", th.IntegerType), 
-        th.Property("WikiPostId", th.IntegerType), 
-    ).to_dict()   
+        th.Property("Id", th.IntegerType),
+        th.Property("TagName", th.StringType),
+        th.Property("Count", th.IntegerType),
+        th.Property("ExcerptPostId", th.IntegerType),
+        th.Property("WikiPostId", th.IntegerType),
+    ).to_dict()
+
 
 class UsersStream(StackOverflowSampleDataStream):
     """Define custom stream."""
@@ -104,13 +106,14 @@ class UsersStream(StackOverflowSampleDataStream):
         th.Property("Id", th.IntegerType),
         th.Property("Reputation", th.IntegerType),
         th.Property("CreationDate", th.DateTimeType),
-        th.Property("DisplayName", th.StringType), 
+        th.Property("DisplayName", th.StringType),
         th.Property("LastAccessDate", th.DateTimeType),
-        th.Property("AboutMe", th.StringType), 
+        th.Property("AboutMe", th.StringType),
         th.Property("Views", th.IntegerType),
         th.Property("UpVotes", th.IntegerType),
         th.Property("DownVotes", th.IntegerType),
     ).to_dict()
+
 
 class VotesStream(StackOverflowSampleDataStream):
     """Define custom stream."""
@@ -120,7 +123,7 @@ class VotesStream(StackOverflowSampleDataStream):
     replication_key = None
     schema = th.PropertiesList(
         th.Property("Id", th.IntegerType),
-        th.Property("PostId", th.IntegerType), 
+        th.Property("PostId", th.IntegerType),
         th.Property("VoteTypeId", th.IntegerType),
         th.Property("CreationDate", th.DateTimeType),
     ).to_dict()
